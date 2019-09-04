b1f4ad5e8caf0811135818bd2cc231652f419a72
VictorBayu <victorbayu85@gmail.com>
2019-09-04 17:30:04 +0700

    menambahkan code jalannya app tebak angka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -13,24 +13,43 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
+	int random;
+	private EditText input_angka;
+	private Button guess_button,reset_button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+		initRandomNumber();
+		input_angka = findViewById(R.id.number_input);
+		guess_button = findViewById(R.id.guess_button);
 	}
 
 	// TODO: generate angka random di sini
-	private void initRandomNumber() {
-
+	private int initRandomNumber() {
+		Random r = new Random();
+		random = r.nextInt(100);
+		return random;
 	}
 
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		int number = Integer.parseInt(String.valueOf(input_angka.getText()));
+		if (number > random){
+			Toast.makeText(this, "Tebakan anda terlalu besar!", Toast.LENGTH_SHORT).show();
+		}else if (number < random){
+			Toast.makeText(this, "Tebakan anda terlalu kecil!", Toast.LENGTH_SHORT).show();
+		}else {
+			guess_button.setEnabled(false);
+			Toast.makeText(this, "Tebakan anda benar", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
+		input_angka.setText("");
+		guess_button.setEnabled(true);
 	}
 }
